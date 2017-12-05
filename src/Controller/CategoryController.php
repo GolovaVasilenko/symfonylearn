16650d6a6923e35714814e34ae3b33a67491f42a
GolovaVasilenko <vasilenko75@gmail.com>
2017-12-05 21:33:34 +0200

    try commit

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class CategoryController extends Controller
+{
+
+	/**
+	 * @Route("/category/{slug}/{page}", name="category_show", requirements={"page": "\d+"})
+	 *
+	 * @param $slug
+	 * @param $page
+	 * $param $session
+	 *
+	 * $return Response
+	 */
+	public function show($slug, SessionInterface $session, Request $request, $page = 1 )
+	{
+		$session->set('lastVisitedCategory', $slug);
+		$param = $request->query->get('param');
+
+		return $this->render('category/show.html.twig',
+			['slug' => $slug,
+			 'title' => "category title",
+			 'h1' => 'category Name',
+			 'param' => $param,
+			 'page' => $page]);
+	}
+
+	/**
+	 * Route("/message", name="category_message")
+	 */
+	public function message(SessionInterface $session)
+	{
+		$this->addFlash('notice', 'Your Message in category');
+		$last_visit = $session->get('lastVisitedCategory');
+		return $this->redirectToRoute('category_show', ['slug' => $last_visit]);
+	}
+}
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,19 +5,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class PageController extends Controller
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="home")
 	 *
 	 * @return mixed
 	 */
 	public function home()
 	{
 		$data['title'] = "Home page";
-		$data['h1'] = "Hom page Welcome to my site";
+		$data['h1'] = "Home page Welcome to my site";
 		$data['body'] = "lorem  ipsum Давно выяснено, что при оценке дизайна и композиции 
 		читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот 
 		обеспечивает более или менее стандартное заполнение шаблона, а также реальное 
@@ -27,14 +29,16 @@ class PageController extends Controller
 	}
 
 	/**
-	 * @Route("/about")
+	 * @Route("/about", name="about")
 	 *
 	 * @return mixed
 	 */
-	public function about()
+	public function about(SessionInterface $session)
 	{
 		$data['title'] = "About Us page";
 		$data['h1'] = "About my super Shop Info";
+		$data['last_visit'] = $session->get('lastVisitedCategory');
+		$data['link'] = $this->generateUrl('category_show', ['slug' => 'notebook', 'param' => 'ddd'], UrlGeneratorInterface::ABSOLUTE_URL);
 		$data['body'] = "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и 
 		вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с 
 		начала XVI века. В то время некий безымянный печатник создал большую коллекцию 
@@ -44,5 +48,13 @@ class PageController extends Controller
 		Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы 
 		электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.";
 		return $this->render('page/about.html.twig', $data);
+	}
+
+	/**
+	 * @Route("/about-to")
+	 */
+	public function redirectToShow()
+	{
+		return $this->redirectToRoute('home');
 	}
 }
