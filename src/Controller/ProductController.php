0166a4b2f62e9ad54834532e306034b45ee16bf7
GolovaVasilenko <vasilenko75@gmail.com>
2017-12-08 21:32:00 +0200

    exemples work with Doctrine

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ProductController extends Controller
+{
+
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index(EntityManagerInterface $em)
+    {
+        $product = new Product();
+        $product->setName("Notebook")
+                ->setPrice(8976.89)
+                ->setDescription("Description for product Notebook");
+
+        $em->persist($product);
+        $em->flush();
+
+        return new Response("New Product Created");
+    }
+
+	/**
+	 * @Route("/product-show/{id}", name="product_show")
+	 */
+    public function show($id)
+    {
+    	$repo = $this->getDoctrine()->getRepository(Product::class);
+    	$product = $repo->find($id);
+
+    	if(!$product){
+		    throw $this->createNotFoundException('Product not found');
+	    }
+
+	    return $this->render('product/show.html.twig', ['product' => $product, 'title' => 'new product']);
+    }
+
+    /**
+	 * @Route("/product-show-by-name/{name}", name="product_show_by_name")
+	 */
+	public function showByName($name)
+	{
+		$repo = $this->getDoctrine()->getRepository(Product::class);
+		$product = $repo->findOneBy(['name' => $name]);
+
+		if(!$product){
+			throw $this->createNotFoundException('Product not found');
+		}
+
+		return $this->render('product/show.html.twig', ['product' => $product, 'title' => 'new product']);
+	}
+
+	/**
+	 * @Route("/products/{name}", name="products_list")
+	 */
+	public function listProduct($name = '')
+	{
+		$repo = $this->getDoctrine()->getRepository(Product::class);
+
+
+		if($name){
+			$products = $repo->findBy(['name' => $name], ['price' => 'DESC']);
+		}
+		else {
+			$products = $repo->findAll();
+		}
+
+		if(!$products){
+			throw $this->createNotFoundException('Products not found');
+		}
+
+		return $this->render('product/list.html.twig',
+			['products' => $products, 'title' => 'list products']);
+	}
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+    private $name;
+
+	/**
+	 * @ORM\Column(type="decimal", scale=2, nullable=true)
+	 */
+    private $price;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+    private $description;
+
+	/**
+	 * @return mixed
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param mixed $id
+	 *
+	 * @return Product
+	 */
+	public function setId( $id ) {
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param mixed $name
+	 *
+	 * @return Product
+	 */
+	public function setName( $name ) {
+		$this->name = $name;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getPrice() {
+		return $this->price;
+	}
+
+	/**
+	 * @param mixed $price
+	 *
+	 * @return Product
+	 */
+	public function setPrice( $price ) {
+		$this->price = $price;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+
+	/**
+	 * @param mixed $description
+	 *
+	 * @return Product
+	 */
+	public function setDescription( $description ) {
+		$this->description = $description;
+		return $this;
+	}
+
+
+
+}
