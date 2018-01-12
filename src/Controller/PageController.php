c671d72ef6085c6d0a6da5198f231d9dfbc51590
GolovaVasilenko <vasilenko75@gmail.com>
2018-01-12 21:08:11 +0200

    create entity Order

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Entity\FeedBack;
 use App\Entity\Page;
 use App\Form\FeedBackType;
 use App\Service\Catalogue;
@@ -16,30 +17,29 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class PageController extends Controller
-{
+class PageController extends Controller {
 	/**
 	 * @Route("/", name="home")
 	 *
 	 * @param StaticPage $sp
 	 * @param Catalogue $catalogue
+	 *
 	 * @return Response
 	 */
-	public function getPage(StaticPage $sp, Catalogue $catalogue)
-	{
+	public function getPage( StaticPage $sp, Catalogue $catalogue ) {
 
 		$products = $catalogue->getProducts();
 
-		$page = $sp->getPage('home');
+		$page = $sp->getPage( 'home' );
 
-		if(!$page){
-			throw new NotFoundHttpException("ERROR 404 Page Not Found!");
+		if ( ! $page ) {
+			throw new NotFoundHttpException( "ERROR 404 Page Not Found!" );
 		}
 
-		return $this->render('page/index.html.twig', [
-								'page' => $page,
-								'products' => $products
-		]);
+		return $this->render( 'page/index.html.twig', [
+			'page'     => $page,
+			'products' => $products
+		] );
 	}
 
 	/**
@@ -48,16 +48,15 @@ class PageController extends Controller
 	 *
 	 * @return Response
 	 */
-	public function about(StaticPage $sp)
-	{
-		$page = $sp->getPage('about');
+	public function about( StaticPage $sp ) {
+		$page = $sp->getPage( 'about' );
 
-		if(!$page){
-			throw new NotFoundHttpException("ERROR 404 Page Not Found!");
+		if ( ! $page ) {
+			throw new NotFoundHttpException( "ERROR 404 Page Not Found!" );
 		}
-		return $this->render('page/page.html.twig', [
+		return $this->render( 'page/page.html.twig', [
 			'page' => $page,
-		]);
+		] );
 	}
 
 	/**
@@ -65,25 +64,45 @@ class PageController extends Controller
 	 * @param StaticPage $sp
 	 *
 	 * @param Request $request
+	 *
 	 * @return Response
 	 */
-	public function contacts(StaticPage $sp, Request $request)
-	{
-		$page = $sp->getPage('contacts');
-		$form = $this->createForm(FeedBackType::class);
+	public function contacts( StaticPage $sp, Request $request, EntityManagerInterface $em, \Swift_Mailer $mailer ) {
+		$page = $sp->getPage( 'contacts' );
 
-		if(!$page){
-			throw new NotFoundHttpException("ERROR 404 Page Not Found!");
+		$feedback = new FeedBack();
+
+		$form = $this->createForm( FeedBackType::class, $feedback );
+		$form->handleRequest( $request );
+
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$em->persist( $feedback );
+			$em->flush();
+			$this->addFlash( 'info', "Спасибо за сообщение!" );
+
+			$message = ( new \Swift_Message( 'Сообщение с сайта Super Mayka' ) )
+				->setFrom( [ getenv( 'MAILER_FROM' ) => getenv( 'MAILER_FROM_NAME' ) ] )
+				->setTo( getenv( 'ADMIN_EMAIL' ) )
+				->setBody(
+					$this->renderView(
+					// templates/emails/registration.html.twig
+						'email/feedback.html.twig',
+						array( 'feedback' => $feedback )
+					),
+					'text/html'
+				);
+			$mailer->send( $message );
+
+			return $this->redirectToRoute( 'contacts' );
 		}
-		return $this->render('page/feedback.html.twig', [
+
+		if ( ! $page ) {
+			throw new NotFoundHttpException( "ERROR 404 Page Not Found!" );
+		}
+		return $this->render( 'page/feedback.html.twig', [
 			'page' => $page,
 			'form' => $form->createView(),
-		]);
+		] );
 	}
 
-
-	/*public function redirectToShow()
-	{
-		return $this->redirectToRoute('home');
-	}*/
 }
---
+++ src/Entity/Order.php
@@ -0,0 +1,278 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Order
+ * @package App\Entity
+ * @ORM\Entity()
+ * @ORM\Table(name="orders")
+ */
+class Order
+{
+
+	/**
+	 * @var int
+	 *
+	 * @ORM\GeneratedValue()
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id()
+	 */
+	private $id;
+
+	/**
+	 * @var User|null
+	 *
+	 * @ORM\ManyToOne(targetEntity="App\Entity\User")
+	 * @ORM\JoinColumn(name="user_id", nullable=true, onDelete="CASCADE")
+	 */
+	private $user;
+
+	/**
+	 * @var \DateTime
+	 *
+	 * @ORM\Column(type="datetime")
+	 */
+	private $createdAt;
+
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(type="integer")
+	 */
+	private $count;
+
+	/**
+	 * @var float
+	 *
+	 * @ORM\Column(type="decimal", scale=2, precision=20)
+	 */
+	private $amount;
+
+
+	const STATUS_DRAFT = 0;
+	const STATUS_ORDER = 1;
+	const STATUS_SEND = 2;
+	const STATUS_RESEIVED = 3;
+	const STATUS_COMPLETED = 4;
+
+	/**
+	 * @var int
+	 *
+	 * @ORM\Column(type="smallint")
+	 */
+	private $status;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $email;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $phone;
+
+	/**
+	 * @var string|null
+	 *
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $address;
+
+	/**
+	 * @var boolean
+	 *
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isPaid;
+
+	public function __construct()
+	{
+		$this->createdAt = new \DateTime();
+		$this->count = 0;
+		$this->amount = 0;
+		$this->email = '';
+		$this->phone = '';
+		$this->status = self::STATUS_DRAFT;
+		$this->isPaid = false;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId(): int {
+		return $this->id;
+	}
+
+	/**
+	 * @param int $id
+	 *
+	 * @return Order
+	 */
+	public function setId( int $id ): Order {
+		$this->id = $id;
+		return $this;
+	}
+
+	/**
+	 * @return User|null
+	 */
+	public function getUser(): ?User {
+		return $this->user;
+	}
+
+	/**
+	 * @param User|null $user
+	 *
+	 * @return Order
+	 */
+	public function setUser( ?User $user ): Order {
+		$this->user = $user;
+		return $this;
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getCreatedAt(): \DateTime {
+		return $this->createdAt;
+	}
+
+	/**
+	 * @param \DateTime $createdAt
+	 *
+	 * @return Order
+	 */
+	public function setCreatedAt( \DateTime $createdAt ): Order {
+		$this->createdAt = $createdAt;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getCount(): int {
+		return $this->count;
+	}
+
+	/**
+	 * @param int $count
+	 *
+	 * @return Order
+	 */
+	public function setCount( int $count ): Order {
+		$this->count = $count;
+		return $this;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getAmount(): float {
+		return $this->amount;
+	}
+
+	/**
+	 * @param float $amount
+	 *
+	 * @return Order
+	 */
+	public function setAmount( float $amount ): Order {
+		$this->amount = $amount;
+		return $this;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getStatus(): int {
+		return $this->status;
+	}
+
+	/**
+	 * @param int $status
+	 *
+	 * @return Order
+	 */
+	public function setStatus( int $status ): Order {
+		$this->status = $status;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEmail(): string {
+		return $this->email;
+	}
+
+	/**
+	 * @param string $email
+	 *
+	 * @return Order
+	 */
+	public function setEmail( string $email ): Order {
+		$this->email = $email;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPhone(): string {
+		return $this->phone;
+	}
+
+	/**
+	 * @param string $phone
+	 *
+	 * @return Order
+	 */
+	public function setPhone( string $phone ): Order {
+		$this->phone = $phone;
+		return $this;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getAddress(): ?string {
+		return $this->address;
+	}
+
+	/**
+	 * @param null|string $address
+	 *
+	 * @return Order
+	 */
+	public function setAddress( ?string $address ): Order {
+		$this->address = $address;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isPaid(): bool {
+		return $this->isPaid;
+	}
+
+	/**
+	 * @param bool $isPaid
+	 *
+	 * @return Order
+	 */
+	public function setIsPaid( bool $isPaid ): Order {
+		$this->isPaid = $isPaid;
+		return $this;
+	}
+
+}
